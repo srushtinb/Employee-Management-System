540176fd0dcc5cbbeb125c6cd70ad85c9f15eef9
srushtinb <srushtiicse@gmail.com>
2025-06-14 15:12:00 +0530

    Initial files

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,14 +3,14 @@ import Header from '../other/Header'
 import TaskListNumbers from '../other/TaskListNumbers'
 import TaskList from '../TaskList/TaskList'
 
-const EmployeeDashboard = () => {
+const EmployeeDashboard = (props) => {
+
   return (
-    <div>
-        <div className='p-20 bg-[#1c1c1c] h-screen'>
-            <Header/>
-            <TaskListNumbers/>
-            <TaskList/>
-        </div>
+    <div className='p-10 bg-[#1C1C1C] h-screen'>
+        
+        <Header changeUser={props.changeUser} data={props.data}/>
+        <TaskListNumbers data={props.data} />
+        <TaskList data={props.data} />
     </div>
   )
 }
