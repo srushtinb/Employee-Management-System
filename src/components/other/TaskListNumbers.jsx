421b2bcf6ae28a735eba719a10fdc3e37e908039
srushtinb <srushtiicse@gmail.com>
2025-06-14 15:15:03 +0530

    Initial files

--- src/components/other/TaskListNumbers.jsx
+++ src/components/other/TaskListNumbers.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
 
-const TaskListNumbers = () => {
+const TaskListNumbers = ({data}) => {
   return (
     <div className='flex mt-10 justify-between gap-5 screen'>
-      <div className='rounded-xl w-[45%] py-6 px-9 bg-red-400'>
-        <h2 className='text-3xl font-semibold'>1</h2>
-        <h3 className='text-xl font-medium'>New Task</h3>
-      </div>
-
-      <div className='rounded-xl w-[45%] py-6 px-9 bg-green-400'>
-        <h2 className='text-3xl font-semibold'>3</h2>
-        <h3 className='text-xl font-medium'>Completed Task</h3>
-      </div>
-
-      <div className='rounded-xl w-[45%] py-6 px-9 bg-blue-400'>
-        <h2 className='text-3xl font-semibold'>0</h2>
-        <h3 className='text-xl font-medium'>Accepted Task</h3>
-      </div>
-
-      <div className='rounded-xl w-[45%] py-6 px-9 bg-yellow-300'>
-        <h2 className='text-3xl font-semibold text-black' >0</h2>
-        <h3 className='text-xl font-medium text-black'>Failed Task</h3>
-      </div>
+        
+        <div className='rounded-xl w-[45%] py-6 px-9 bg-blue-400'>
+            <h2 className='text-3xl font-bold'>{data.taskCounts.newTask}</h2>
+            <h3 className='text-xl mt-0.5 font-medium'>New Task</h3>
+        </div>
+        <div className='rounded-xl w-[45%] py-6 px-9 bg-green-400'>
+            <h2 className='text-3xl font-bold'>{data.taskCounts.completed}</h2>
+            <h3 className='text-xl mt-0.5 font-medium'>Completed Task</h3>
+        </div>
+        <div className='rounded-xl w-[45%] py-6 px-9 bg-yellow-400 '>
+            <h2 className='text-3xl text-black font-bold'>{data.taskCounts.active}</h2>
+            <h3 className='text-xl mt-0.5 text-black font-medium'>Accepted Task</h3>
+        </div>
+        <div className='rounded-xl w-[45%] py-6 px-9 bg-red-400'>
+            <h2 className='text-3xl font-bold'>{data.taskCounts.failed}</h2>
+            <h3 className='text-xl mt-0.5 font-medium'>Failed Task</h3>
+        </div>
     </div>
   )
 }
